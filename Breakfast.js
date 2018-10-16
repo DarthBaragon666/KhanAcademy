9bff489b80479eca812ba05113efd5f1dd2b6c88
Darth Baragon AKA Sean <38866030+DarthBaragon666@users.noreply.github.com>
2018-10-16 10:28:14 -0400

    Create Breakfast.js

---
+++ Breakfast.js
@@ -0,0 +1,10 @@
+background(186, 145, 20); // wooden table
+fill(255, 0, 0);
+ellipse(200, 200, 350, 350); // plate
+fill(255, 255, 255);
+ellipse(200, 200, 300, 300); 
+ellipse(147, 153, 150, 150);//Egg white 
+fill(214, 214, 8);
+ellipse(147, 153, 100, 100);//Egg yolk
+fill(166, 2, 2);
+rect(232, 76, 42, 220);//Bacon
