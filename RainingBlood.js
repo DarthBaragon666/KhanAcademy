e99411e30801ceb0f2a701b0bd16821ae3efd5d4
Darth Baragon AKA Sean <38866030+DarthBaragon666@users.noreply.github.com>
2018-11-15 11:04:48 -0500

    Create RainingBlood.js

---
+++ RainingBlood.js
@@ -0,0 +1,19 @@
+var xPositions = [200];
+var yPositions = [0];
+
+draw = function() {
+    background(204, 247, 255);
+
+    for (var i = 0; i < xPositions.length; i++) {
+        noStroke();
+        fill(255, 0, 0);
+        ellipse(xPositions[i], yPositions[i], 10, 10);
+        yPositions[i] += 5;
+        if (yPositions[i] > 400) {
+            yPositions[i] = 0;
+        }
+    }
+    var randomX = random(0, 400);
+    xPositions.push(randomX);
+    yPositions.push(0);
+};
